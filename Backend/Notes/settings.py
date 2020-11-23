53dc767bd4ab64e859575abad5c99327bcd32ce9
omer <omkob@hotmail.com>
2020-11-23 20:37:42 +0100

    Increased access and refresh token time

--- Backend/Notes/settings.py
+++ Backend/Notes/settings.py
@@ -141,8 +141,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    # 'REFRESH_TOKEN_LIFETIME': timedelta(minutes=10),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
